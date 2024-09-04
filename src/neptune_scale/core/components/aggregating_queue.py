f178d21904236583fcca20c5dcfbc69c7ccb150f
Rafal Jankowski <rafal.jankowski@neptune.ai>
2024-09-04 16:07:59 +0200

    Preparations

--- src/neptune_scale/core/components/aggregating_queue.py
+++ src/neptune_scale/core/components/aggregating_queue.py
@@ -9,23 +9,22 @@ from queue import (
 from threading import RLock
 from typing import Optional
 
+from neptune_api.proto.neptune_pb.ingest.v1.pub.ingest_pb2 import RunOperation
+
 from neptune_scale.core.components.abstract import Resource
 from neptune_scale.core.components.queue_element import QueueElement
-from neptune_scale.parameters import (
-    MAX_BATCH_SIZE,
-    MAX_QUEUE_ELEMENT_SIZE,
-)
+from neptune_scale.parameters import MAX_QUEUE_ELEMENT_SIZE
 
 
 class AggregatingQueue(Resource):
     def __init__(
         self,
         max_queue_size: int,
-        max_batch_size: int = MAX_BATCH_SIZE,
+        max_elements_in_batch: int = 1,  # TODO: Restore default value to MAX_BATCH_SIZE
         max_queue_element_size: int = MAX_QUEUE_ELEMENT_SIZE,
     ) -> None:
         self._max_queue_size = max_queue_size
-        self._max_batch_size = max_batch_size
+        self._max_elements_in_batch = max_elements_in_batch
         self._max_queue_element_size = max_queue_element_size
 
         self._queue: Queue[QueueElement] = Queue(maxsize=max_queue_size)
@@ -41,6 +40,8 @@ class AggregatingQueue(Resource):
             self._queue.put_nowait(element)
 
     def _get_next(self) -> Optional[QueueElement]:
+        # We can assume that each of queue elements are less than MAX_QUEUE_ELEMENT_SIZE
+        # We can assume that every queue element has the same project, run id and family
         if self._latest_unprocessed is not None:
             return self._latest_unprocessed
 
@@ -50,9 +51,42 @@ class AggregatingQueue(Resource):
         except Empty:
             return None
 
+    def commit(self) -> None:
+        self._latest_unprocessed = None
+
     def get_nowait(self) -> QueueElement:
         with self._lock:
-            element = self._get_next()
-            if element is None:
+            elements_in_batch: int = 0
+            batch: Optional[RunOperation] = None
+            batch_sequence_id: Optional[int] = None
+            batch_timestamp: Optional[float] = None
+
+            while (element := self._get_next()) is not None:
+                elements_in_batch += 1
+
+                if elements_in_batch > self._max_elements_in_batch:
+                    break
+
+                if batch is None:
+                    batch = RunOperation()
+                    batch.ParseFromString(element.operation)
+                    batch_sequence_id, batch_timestamp = element.sequence_id, element.timestamp
+                    self.commit()
+                else:
+                    # TODO: Check if steps and timestamps are the same
+                    # TODO: Merge if size allows
+                    # TODO: Update batch sequence_id and timestamp
+                    pass
+                    break
+
+            if batch is None:
                 raise Empty
-            return element
+
+            assert batch_sequence_id is not None
+            assert batch_timestamp is not None
+
+            return QueueElement(
+                sequence_id=batch_sequence_id,
+                timestamp=batch_timestamp,
+                operation=batch.SerializeToString(),
+            )
--- tests/unit/test_aggregating_queue.py
+++ tests/unit/test_aggregating_queue.py
@@ -19,8 +19,9 @@ from neptune_scale.core.components.queue_element import QueueElement
 @freeze_time("2024-09-01")
 def test__simple():
     # given
-    update = UpdateRunSnapshot(assign={f"key_{i}": Value(string=("a" * 1024)) for i in range(1024)})
-    element = QueueElement(sequence_id=1, timestamp=time.process_time(), operation=RunOperation(update=update))
+    update = UpdateRunSnapshot(assign={f"key_{i}": Value(string=("a" * 2)) for i in range(2)})
+    operation = RunOperation(update=update)
+    element = QueueElement(sequence_id=1, timestamp=time.process_time(), operation=operation.SerializeToString())
 
     # and
     queue = AggregatingQueue(max_queue_size=1)
@@ -35,19 +36,23 @@ def test__simple():
 @freeze_time("2024-09-01")
 def test__max_size_exceeded():
     # given
+    operation1 = RunOperation()
+    operation2 = RunOperation()
+    element1 = QueueElement(sequence_id=1, timestamp=time.process_time(), operation=operation1.SerializeToString())
+    element2 = QueueElement(sequence_id=2, timestamp=time.process_time(), operation=operation2.SerializeToString())
+
+    # and
     queue = AggregatingQueue(max_queue_size=1)
 
     # when
-    queue.put_nowait(element=QueueElement(sequence_id=1, timestamp=time.process_time(), operation=RunOperation()))
+    queue.put_nowait(element=element1)
 
     # then
     assert True
 
     # when
     with pytest.raises(Full):
-        queue.put_nowait(
-            element=QueueElement(sequence_id=2, timestamp=time.process_time() + 1, operation=RunOperation())
-        )
+        queue.put_nowait(element=element2)
 
 
 @freeze_time("2024-09-01")
@@ -58,3 +63,62 @@ def test__empty():
     # when
     with pytest.raises(Empty):
         _ = queue.get_nowait()
+
+
+@freeze_time("2024-09-01")
+def test__batch_size_limit():
+    # given
+    update1 = UpdateRunSnapshot(step=None, assign={f"aa{i}": Value(int64=(i * 97)) for i in range(2)})
+    update2 = UpdateRunSnapshot(step=None, assign={f"bb{i}": Value(int64=(i * 25)) for i in range(2)})
+    operation1 = RunOperation(update=update1)
+    operation2 = RunOperation(update=update2)
+    element1 = QueueElement(sequence_id=1, timestamp=time.process_time(), operation=operation1.SerializeToString())
+    element2 = QueueElement(sequence_id=2, timestamp=time.process_time(), operation=operation2.SerializeToString())
+
+    # and
+    queue = AggregatingQueue(max_queue_size=2, max_elements_in_batch=1)
+
+    # when
+    queue.put_nowait(element=element1)
+    queue.put_nowait(element=element2)
+
+    # then
+    assert queue.get_nowait() == element1
+    assert queue.get_nowait() == element2
+
+
+@freeze_time("2024-09-01")
+def test__batching():
+    # given
+    update1 = UpdateRunSnapshot(step=None, assign={f"aa{i}": Value(int64=(i * 97)) for i in range(2)})
+    update2 = UpdateRunSnapshot(step=None, assign={f"bb{i}": Value(int64=(i * 25)) for i in range(2)})
+
+    # and
+    operation1 = RunOperation(update=update1, project="project", run_id="run_id")
+    operation2 = RunOperation(update=update2, project="project", run_id="run_id")
+
+    # and
+    element1 = QueueElement(sequence_id=1, timestamp=time.process_time(), operation=operation1.SerializeToString())
+    element2 = QueueElement(sequence_id=2, timestamp=time.process_time(), operation=operation2.SerializeToString())
+
+    # and
+    queue = AggregatingQueue(max_queue_size=2, max_elements_in_batch=2)
+
+    # and
+    queue.put_nowait(element=element1)
+    queue.put_nowait(element=element2)
+
+    # when
+    result = queue.get_nowait()
+
+    # then
+    assert result.sequence_id == 2
+    assert result.timestamp == element2.timestamp
+
+    # and
+    batch = RunOperation()
+    batch.ParseFromString(result.operation)
+
+    assert batch.project == "project"
+    assert batch.run_id == "run_id"
+    assert all(k in batch.update.assign for k in ["aa0", "aa1", "bb0", "bb1"])
