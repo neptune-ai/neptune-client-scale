0f93b98e7d7f70b2107b969958b1c30624c963f0
Rafal Jankowski <rafal.jankowski@neptune.ai>
2024-09-04 16:19:27 +0200

    SingleOperation

--- src/neptune_scale/core/components/queue_element.py
+++ src/neptune_scale/core/components/queue_element.py
@@ -1,9 +1,25 @@
-__all__ = ("BatchedOperations",)
+__all__ = ("BatchedOperations", "SingleOperation")
 
 from typing import NamedTuple
 
 
 class BatchedOperations(NamedTuple):
+    # Operation identifier of the last operation in the batch
     sequence_id: int
+    # Timestamp of the last operation in the batch
     timestamp: float
+    # Protobuf serialized (RunOperation)
     operation: bytes
+
+
+class SingleOperation(NamedTuple):
+    # Operation identifier
+    sequence_id: int
+    # Timestamp of the operation being enqueued
+    timestamp: float
+    # Protobuf serialized (RunOperation)
+    operation: bytes
+    # Size of the metadata in the operation (without project, family, run_id etc.)
+    metadata_size: int
+    # Whether the operation is metadata update or not (run creation)
+    is_metadata_update: bool
