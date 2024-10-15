3cb54d27aa127168b9bd510e4893d297908f0fab
Siddhant Sadangi <siddhant.sadangi@neptune.ai>
2024-10-15 18:28:04 +0200

    Update __init__.py

--- src/neptune_scale/__init__.py
+++ src/neptune_scale/__init__.py
@@ -402,9 +402,9 @@ class Run(WithResources, AbstractContextManager):
 
     def log_metrics(
         self,
-        step: Optional[Union[float, int]] = None,
+        data: Dict[str, Union[float, int]],
+        step: Union[float, int],
         timestamp: Optional[datetime] = None,
-        data: Optional[Dict[str, Union[float, int]]] = None,
     ) -> None:
         """
         Logs the specified metrics to a Neptune run.
@@ -419,14 +419,12 @@ class Run(WithResources, AbstractContextManager):
         Use namespaces to structure the metadata into meaningful categories.
 
         Args:
-            step: Index of the log entry. Must be increasing.
-                If not specified, the log_metrics() call increments the step starting from the highest
-                already logged value.
-                Tip: Using float rather than int values can be useful, for example, when logging substeps in a batch.
-            timestamp: Time of logging the metadata.
             data: Dictionary of metrics to log.
                 Each metric value is associated with a step.
                 To log multiple metrics at once, pass multiple key-value pairs.
+            step: Index of the log entry. Must be increasing.
+                Tip: Using float rather than int values can be useful, for example, when logging substeps in a batch.
+            timestamp (optional): Time of logging the metadata.
 
 
         Examples:
@@ -435,8 +433,8 @@ class Run(WithResources, AbstractContextManager):
 
             with Run(...) as run:
                 run.log_metrics(
-                    step=1.2,
                     data={"loss": 0.14, "acc": 0.78},
+                    step=1.2,
                 )
             ```
         """
