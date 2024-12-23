3d83f76e0b31898248c27a439a8b68aa805c9c6c
Krzysztof Godlewski <krzysztof.godlewski@neptune.ai>
2024-12-23 21:43:05 +0100

    Add `envs.BASE_STORAGE_DIR` and `get_str()`

--- src/neptune_scale/util/envs.py
+++ src/neptune_scale/util/envs.py
@@ -14,6 +14,7 @@
 # limitations under the License.
 
 import os
+from typing import Optional
 
 PROJECT_ENV_NAME = "NEPTUNE_PROJECT"
 
@@ -28,6 +29,12 @@ SUBPROCESS_KILL_TIMEOUT = "NEPTUNE_SUBPROCESS_KILL_TIMEOUT"
 ALLOW_SELF_SIGNED_CERTIFICATE = "NEPTUNE_ALLOW_SELF_SIGNED_CERTIFICATE"
 SKIP_NON_FINITE_METRICS = "NEPTUNE_SKIP_NON_FINITE_METRICS"
 
+BASE_STORAGE_DIR = "NEPTUNE_BASE_STORAGE_DIR"
+
 
 def get_bool(name: str, default: bool) -> bool:
     return os.getenv(name, str(default)).lower() in ("true", "1")
+
+
+def get_str(name: str, default: Optional[str] = None) -> Optional[str]:
+    return os.getenv(name, default)
