ae1d505da74bc73e206ea2e5cd3d2d9a2127bf6a
Krzysztof Godlewski <krzysztof.godlewski@neptune.ai>
2025-01-09 14:54:14 +0100

    Extract `raise_for_http_status()` to `net.util`

---
+++ src/neptune_scale/net/util.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from neptune_scale.exceptions import (
+    NeptuneConnectionLostError,
+    NeptuneInternalServerError,
+    NeptuneTooManyRequestsResponseError,
+    NeptuneUnauthorizedError,
+    NeptuneUnexpectedResponseError,
+)
+from neptune_scale.util import get_logger
+
+logger = get_logger()
+
+
+def raise_for_http_status(status_code: int) -> None:
+    assert status_code >= 400, f"Status code {status_code} is not an error"
+
+    logger.error("HTTP response error: %s", status_code)
+    if status_code == 403:
+        raise NeptuneUnauthorizedError()
+    elif status_code == 408:
+        raise NeptuneConnectionLostError()
+    elif status_code == 429:
+        raise NeptuneTooManyRequestsResponseError()
+    elif status_code // 100 == 5:
+        raise NeptuneInternalServerError()
+    else:
+        raise NeptuneUnexpectedResponseError()
--- src/neptune_scale/sync/sync_process.py
+++ src/neptune_scale/sync/sync_process.py
@@ -37,7 +37,6 @@ from neptune_scale.exceptions import (
     NeptuneAttributeTypeUnsupported,
     NeptuneConnectionLostError,
     NeptuneFloatValueNanInfUnsupported,
-    NeptuneInternalServerError,
     NeptuneOperationsQueueMaxSizeExceeded,
     NeptuneProjectInvalidName,
     NeptuneProjectNotFound,
@@ -54,16 +53,14 @@ from neptune_scale.exceptions import (
     NeptuneStringSetExceedsSizeLimit,
     NeptuneStringValueExceedsSizeLimit,
     NeptuneSynchronizationStopped,
-    NeptuneTooManyRequestsResponseError,
-    NeptuneUnauthorizedError,
     NeptuneUnexpectedError,
-    NeptuneUnexpectedResponseError,
 )
 from neptune_scale.net.api_client import (
     ApiClient,
     backend_factory,
     with_api_errors_handling,
 )
+from neptune_scale.net.util import raise_for_http_status
 from neptune_scale.sync.aggregating_queue import AggregatingQueue
 from neptune_scale.sync.errors_tracking import ErrorsQueue
 from neptune_scale.sync.parameters import (
@@ -406,7 +403,7 @@ class SenderThread(Daemon, WithResources):
 
         status_code = response.status_code
         if status_code != 200:
-            _raise_exception(status_code)
+            raise_for_http_status(status_code)
 
         return response.parsed
 
@@ -446,20 +443,6 @@ class SenderThread(Daemon, WithResources):
                 self._last_queued_seq.notify_all()
             self.interrupt()
             raise NeptuneSynchronizationStopped() from e
-
-
-def _raise_exception(status_code: int) -> None:
-    logger.error("HTTP response error: %s", status_code)
-    if status_code == 403:
-        raise NeptuneUnauthorizedError()
-    elif status_code == 408:
-        raise NeptuneConnectionLostError()
-    elif status_code == 429:
-        raise NeptuneTooManyRequestsResponseError()
-    elif status_code // 100 == 5:
-        raise NeptuneInternalServerError()
-    else:
-        raise NeptuneUnexpectedResponseError()
 
 
 class StatusTrackingThread(Daemon, WithResources):
@@ -508,7 +491,7 @@ class StatusTrackingThread(Daemon, WithResources):
         status_code = response.status_code
 
         if status_code != 200:
-            _raise_exception(status_code)
+            raise_for_http_status(status_code)
 
         return response.parsed
 
