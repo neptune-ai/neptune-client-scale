ff0332f7ba1eccba822ac52501ca2e80ce69b179
Krzysztof Godlewski <krzysztof.godlewski@neptune.ai>
2025-01-14 18:01:08 +0100

    Make the output of `neptune status` more human-readable

--- src/neptune_scale/cli/misc.py
+++ src/neptune_scale/cli/misc.py
@@ -25,12 +25,13 @@ from neptune_scale.storage.operations import list_runs
 
 
 @click.command()
-@click.option("-v", "--verbose", is_flag=True, help="Enable verbose output")
 @click.pass_context
-def status(ctx: click.Context, verbose: bool) -> None:
+def status(ctx: click.Context) -> None:
+    """Display information on runs stored locally."""
     if not is_neptune_dir(ctx.obj["neptune_dir"]):
         sys.exit(1)
 
     for run in list_runs(ctx.obj["neptune_dir"]):
-        line = format_local_run(run, verbose)
-        print(line)
+        print(f"Run {run.run_id}:")
+        line = format_local_run(run, with_run_id=False)
+        print(line, end="\n\n")
--- src/neptune_scale/cli/util.py
+++ src/neptune_scale/cli/util.py
@@ -50,21 +50,27 @@ def format_duration(seconds: int) -> str:
     return " ".join(parts)
 
 
-def format_local_run(run: LocalRun, verbose: bool = False) -> str:
+def format_local_run(run: LocalRun, with_run_id: bool = True) -> str:
     if run.operation_count:
         pct = round(run.last_synced_operation / run.operation_count * 100, 2)
     else:
         pct = 100.0
 
-    parts = [f"Run ID: {run.run_id}, {run.last_synced_operation}/{run.operation_count} ({pct}%) synced"]
+    parts = [
+        f"Project: {run.project}",
+        f"Synced: {run.last_synced_operation}/{run.operation_count} ({pct}%)",
+        f"Created at: {run.creation_time}",
+    ]
+
+    if with_run_id:
+        parts.insert(0, f"Run ID: {run.run_id}")
 
     if run.experiment_name:
         parts.append(f"Experiment: {run.experiment_name}")
     if run.fork_run_id:
         parts.append(f"Forked from `{run.fork_run_id}` at step {run.fork_step}")
 
-    line = ", ".join(parts)
-    if verbose:
-        line = f"{run.path}: Project: {run.project}, Created At: {run.creation_time}, {line}"
+    parts.append(f"Path: {run.path}")
 
-    return line
+    text = "\n".join(f"  - {part}" for part in parts)
+    return text
