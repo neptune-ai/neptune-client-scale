c371430a1f0302b32bd6ae7e9433b6c19939e1ca
Krzysztof Godlewski <krzysztof.godlewski@neptune.ai>
2025-01-16 14:41:29 +0100

    Add `NeptuneValidationError`
    Make some of existing errors inherit the new one

--- src/neptune_scale/sync/sync_process.py
+++ src/neptune_scale/sync/sync_process.py
@@ -114,26 +114,29 @@ logger = get_logger()
 
 CODE_TO_ERROR: dict[IngestCode.ValueType, Optional[type[Exception]]] = {
     IngestCode.OK: None,
+    # Direct NeptuneScaleError descendants
     IngestCode.PROJECT_NOT_FOUND: NeptuneProjectNotFound,
     IngestCode.PROJECT_INVALID_NAME: NeptuneProjectInvalidName,
     IngestCode.RUN_NOT_FOUND: NeptuneRunNotFound,
-    IngestCode.RUN_DUPLICATE: NeptuneRunDuplicate,
+    IngestCode.FIELD_PATH_NON_WRITABLE: NeptuneAttributePathNonWritable,
     IngestCode.RUN_CONFLICTING: NeptuneRunConflicting,
     IngestCode.RUN_FORK_PARENT_NOT_FOUND: NeptuneRunForkParentNotFound,
     IngestCode.RUN_INVALID_CREATION_PARAMETERS: NeptuneRunInvalidCreationParameters,
-    IngestCode.FIELD_PATH_EXCEEDS_SIZE_LIMIT: NeptuneAttributePathExceedsSizeLimit,
+    # NeptuneValidationError descendants
     IngestCode.FIELD_PATH_EMPTY: NeptuneAttributePathEmpty,
     IngestCode.FIELD_PATH_INVALID: NeptuneAttributePathInvalid,
-    IngestCode.FIELD_PATH_NON_WRITABLE: NeptuneAttributePathNonWritable,
+    IngestCode.FIELD_PATH_EXCEEDS_SIZE_LIMIT: NeptuneAttributePathExceedsSizeLimit,
     IngestCode.FIELD_TYPE_UNSUPPORTED: NeptuneAttributeTypeUnsupported,
     IngestCode.FIELD_TYPE_CONFLICTING: NeptuneAttributeTypeMismatch,
-    IngestCode.SERIES_POINT_DUPLICATE: NeptuneSeriesPointDuplicate,
     IngestCode.SERIES_STEP_NON_INCREASING: NeptuneSeriesStepNonIncreasing,
     IngestCode.SERIES_STEP_NOT_AFTER_FORK_POINT: NeptuneSeriesStepNotAfterForkPoint,
     IngestCode.SERIES_TIMESTAMP_DECREASING: NeptuneSeriesTimestampDecreasing,
     IngestCode.FLOAT_VALUE_NAN_INF_UNSUPPORTED: NeptuneFloatValueNanInfUnsupported,
     IngestCode.STRING_VALUE_EXCEEDS_SIZE_LIMIT: NeptuneStringValueExceedsSizeLimit,
     IngestCode.STRING_SET_EXCEEDS_SIZE_LIMIT: NeptuneStringSetExceedsSizeLimit,
+    # NeptuneScaleWarning inheritances
+    IngestCode.RUN_DUPLICATE: NeptuneRunDuplicate,
+    IngestCode.SERIES_POINT_DUPLICATE: NeptuneSeriesPointDuplicate,
 }
 
 
