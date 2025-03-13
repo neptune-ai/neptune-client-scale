6c1b6278437d256cdb0b03a6447fc3fa6cbc69ce
Piotr Łusakowski <piotr.lusakowski@neptune.ai>
2025-03-13 21:56:08 +0100

    Set PRAGMA synchronous=FULL for durability

--- src/neptune_scale/sync/operations_repository.py
+++ src/neptune_scale/sync/operations_repository.py
@@ -393,6 +393,7 @@ class OperationsRepository:
                 )
 
                 self._connection.execute("PRAGMA journal_mode = WAL")
+                self._connection.execute("PRAGMA synchronous = FULL")
 
                 logger.debug(f"Created new SQLite connection for {self._db_path}")
 
