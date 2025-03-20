c9b6bf8e877980838c0f77e86bee87f8d94bf69b
Michał Sośnicki <michal.sosnicki@neptune.ai>
2025-03-20 05:20:15 +0100

    feat: run ErrorsMonitor final time on close

--- src/neptune_scale/sync/supervisor.py
+++ src/neptune_scale/sync/supervisor.py
@@ -5,6 +5,7 @@ __all__ = ("ProcessSupervisor",)
 from collections.abc import Callable
 from multiprocessing import Process
 
+from neptune_scale.sync.parameters import PROCESS_SUPERVISOR_THREAD_SLEEP_TIME
 from neptune_scale.util import (
     Daemon,
     get_logger,
@@ -15,7 +16,7 @@ logger = get_logger()
 
 class ProcessSupervisor(Daemon):
     def __init__(self, process: Process, callback: Callable[[], None]) -> None:
-        super().__init__(name="ProcessSupervisor", sleep_time=0.5)
+        super().__init__(name="ProcessSupervisor", sleep_time=PROCESS_SUPERVISOR_THREAD_SLEEP_TIME)
         self._process: Process = process
         self._callback: Callable[[], None] = callback
 
