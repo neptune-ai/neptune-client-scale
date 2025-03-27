c05d30727cbb98161e3339b65f35b82c42ba2e3d
Piotr Łusakowski <piotr.lusakowski@neptune.ai>
2025-03-27 14:44:40 +0100

    Optimize dict validation

--- src/neptune_scale/api/validation.py
+++ src/neptune_scale/api/validation.py
@@ -4,8 +4,9 @@ __all__ = (
     "verify_type",
     "verify_non_empty",
     "verify_max_length",
+    "verify_dict_type",
+    "verify_value_between",
     "verify_project_qualified_name",
-    "verify_collection_type",
 )
 
 from typing import (
@@ -16,21 +17,17 @@ from typing import (
 
 
 def get_type_name(var_type: Union[type, tuple]) -> str:
-    return var_type.__name__ if hasattr(var_type, "__name__") else str(var_type)
+    if isinstance(var_type, tuple):
+        return " or ".join(get_type_name(t) for t in var_type)
+    elif hasattr(var_type, "__name__"):
+        return var_type.__name__
+    else:
+        return str(var_type)
 
 
 def verify_type(var_name: str, var: Any, expected_type: Union[type, tuple]) -> None:
     if not isinstance(var, expected_type):
-        try:
-            if isinstance(expected_type, tuple):
-                type_name = " or ".join(get_type_name(t) for t in expected_type)
-            else:
-                type_name = get_type_name(expected_type)
-        except Exception as e:
-            # Just to be sure that nothing weird will be raised here
-            raise TypeError(f"Incorrect type of {var_name}") from e
-
-        raise TypeError(f"{var_name} must be a {type_name} (was {type(var)})")
+        raise TypeError(f"{var_name} must be a {get_type_name(expected_type)} (was {type(var)})")
 
 
 def verify_non_empty(var_name: str, var: Any) -> None:
@@ -53,35 +50,24 @@ def verify_project_qualified_name(var_name: str, var: Any) -> None:
         raise ValueError(f"{var_name} is not in expected format, should be 'workspace-name/project-name")
 
 
-def verify_collection_type(
-    var_name: str, var: Union[list, set, tuple], expected_type: Union[type, tuple], allow_none: bool = True
-) -> None:
-    if var is None:
-        if not allow_none:
-            raise ValueError(f"{var_name} must not be None")
-        return
-
-    verify_type(var_name, var, (list, set, tuple))
-
-    for value in var:
-        verify_type(f"elements of collection '{var_name}'", value, expected_type)
-
-
 def verify_dict_type(
     var_name: str, var: Optional[dict[Any, Any]], expected_type: Union[type, tuple], allow_none: bool = True
 ) -> None:
     if var is None:
-        if not allow_none:
-            raise ValueError(f"{var_name} must not be None")
-        return
-
+        if allow_none:
+            return
+        raise ValueError(f"{var_name} must not be None")
     verify_type(var_name, var, dict)
 
+    _is_instance = isinstance  # local binding, faster in tight loops
     for key, value in var.items():
-        if not isinstance(key, str):
+        if not _is_instance(key, str):
             raise TypeError(f"Keys of dictionary '{var_name}' must be strings (got `{key}`)")
 
-        verify_type(f"Values of dictionary '{var_name}'", value, expected_type)
+        if not _is_instance(value, expected_type):
+            raise TypeError(
+                f"Values of dictionary '{var_name}' must be {get_type_name(expected_type)} (got `{key}`:`{value}`)"
+            )
 
 
 def verify_value_between(
