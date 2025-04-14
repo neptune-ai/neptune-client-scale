f1a61c0180699b1306dc9a03bf173760e25d98ae
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2025-04-14 14:55:19 +0200

    tests: removed example from docstrings

--- src/neptune_scale/utils.py
+++ src/neptune_scale/utils.py
@@ -16,19 +16,6 @@ def stringify_unsupported(d: dict[str, Any], **kwargs: Any) -> dict[str, Any]:  
     Returns:
         dict: Flattened dictionary with string keys and cast values
 
-    Example:
-        >>> from neptune_scale import Run
-        >>> run = Run()
-        >>> config = {"mixed_nested": {"list": [1, {"a": 2}, 3, None], "dict": {"a": [1, 2], "b": {"c": 3}, "d": None}}}
-        >>> # without `stringify_unsupported()`
-        >>> run.log_configs(config)  # doctest: +SKIP
-        neptune:WARNING: Dropping value. Config values must be float, bool, int, str, datetime, list, set or tuple (got `mixed_nested`:`{'list': [1, {'a': 2}, 3, None], 'dict': {'a': [1, 2], 'b': {'c': 3}}}`)
-        >>> # with `stringify_unsupported()`
-        >>> from neptune_scale.utils import stringify_unsupported
-        >>> run.log_configs(stringify_unsupported(config))  # Logged successfully
-
-
-
     For more details, see https://docs.neptune.ai/api/utils/#stringify_unsupported
     """
     if not isinstance(d, dict):
