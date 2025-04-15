d1382c9a01e8351c6e75ce1a0616bca3cef76c4f
Michał Sośnicki <michal.sosnicki@neptune.ai>
2025-04-15 10:57:30 +0200

    Add a series of test cases for files

--- tests/e2e/test_log_and_fetch.py
+++ tests/e2e/test_log_and_fetch.py
@@ -1,5 +1,6 @@
 import math
 import os
+import pathlib
 import threading
 import time
 from datetime import (
@@ -10,10 +11,11 @@ from datetime import (
 import numpy as np
 import pytest
 from neptune_fetcher import ReadOnlyRun
-from neptune_fetcher.alpha import runs
+from neptune_fetcher.alpha import runs, filters
 from pytest import mark
 
 from neptune_scale.api.run import Run
+from neptune_scale.api.types import File
 
 from .conftest import (
     random_series,
@@ -162,20 +164,6 @@ def test_single_non_finite_metric(value, run, ro_run):
     assert path not in refresh(ro_run).field_names
 
 
-def test_single_file(run, run_init_kwargs, temp_dir):
-    # given
-    files = {"test_files/file": b"Hello world"}
-
-    # when
-    run.assign_files(files)
-    run.wait_for_processing(SYNC_TIMEOUT)
-
-    # then
-    assert run._operations_repo.get_file_upload_requests_count() == 0
-    runs.download_files(runs=run_init_kwargs["run_id"], attributes="test_files/file", destination=temp_dir)
-    # todo: check the file content
-
-
 def test_async_lag_callback():
     event = threading.Event()
     with Run(
@@ -199,3 +187,108 @@ def test_async_lag_callback():
         # Second callback should be called after logging configs
         event.wait(timeout=60)
         assert event.is_set()
+
+@pytest.mark.parametrize(
+    "files",
+    [
+        {"test_files/file_txt1": b"bytes content"},
+        {"test_files/file_txt2": "tests/e2e/resources/file.txt"},
+        {"test_files/file_txt3": pathlib.Path("tests/e2e/resources/file.txt")},
+        {"test_files/file_txt4": pathlib.Path("tests/e2e/resources/file.txt").absolute()},
+        {"test_files/file_txt5": File(source="tests/e2e/resources/file.txt")},
+        {"test_files/file_txt6": File(source="tests/e2e/resources/file.txt", mime_type="application/json")},
+        {"test_files/file_txt7": File(source=pathlib.Path("tests/e2e/resources/file.txt"), mime_type="application/json")},
+        {"test_files/file_txt8": "tests/e2e/resources/link_file"},
+        {"test_files/file_binary1": "tests/e2e/resources/binary_file"},
+        {"test_files/file_binary2": pathlib.Path("tests/e2e/resources/binary_file")},
+        {"test_files/file_binary3": File(source="tests/e2e/resources/binary_file")},
+        {"test_files/file_binary4": File(source="tests/e2e/resources/binary_file", mime_type="audio/mpeg")},
+        {"test_files/file_binary5": File(source=pathlib.Path("tests/e2e/resources/binary_file"), mime_type="audio/mpeg")},
+        {"test_files/file_multiple1a": "tests/e2e/resources/file.txt", "test_files/file_multiple1b": "tests/e2e/resources/file.txt"},
+        {"test_files/file_multiple2a": "tests/e2e/resources/file.txt", "test_files/file_multiple2b": "tests/e2e/resources/binary_file", "test_files/file_multiple2c": b"bytes content"},
+        {"test_files/汉字Пр\U00009999/file_txt2": "tests/e2e/resources/file.txt"},  # todo: are there invalid characters? add an error case
+        {"test_files/file_path_length1" + "a" * 3 * 1024: "tests/e2e/resources/text_file.txt"},  # just below file metadata limit
+        {"test_files/file_large1": b"a" * (10 * 1024 * 1024)},
+        {"test_files/file_empty1": "tests/e2e/resources/empty_file"}
+    ],
+)
+def test_assign_files(run, run_init_kwargs, temp_dir, files):
+    # when
+    run.assign_files(files)
+    run.wait_for_processing(SYNC_TIMEOUT)
+
+    # then
+    attributes = list(files.keys())
+    runs.download_files(runs=run_init_kwargs["run_id"], attributes=filters.AttributeFilter(name_eq=attributes), destination=temp_dir)
+
+    # check content
+    for attribute, expected in files.items():
+        file_path = os.path.join(temp_dir, attribute)
+        assert os.path.exists(file_path), f"File {file_path} does not exist"
+        if isinstance(expected, bytes):
+            with open(file_path, "rb") as f:
+                content = f.read()
+            assert content == expected, f"Content of {file_path} does not match"
+        else:
+            with open(file_path, "r") as f:
+                content = f.read()
+            with open(expected, "r") as f:
+                expected_content = f.read()
+            assert content == expected_content, f"Content of {file_path} does not match"
+
+
+@pytest.mark.parametrize(
+    "files",
+    [
+        {"test_files/file_destination1": File(source="tests/e2e/resources/file.txt", destination="custom_destination.txt")},
+    ]
+)
+def test_assign_files_destination(run, run_init_kwargs, temp_dir, files):
+    # when
+    run.assign_files(files)
+    run.wait_for_processing(SYNC_TIMEOUT)
+
+    # then
+    attributes = list(files.keys())
+    df = runs.fetch_runs_table(runs=run_init_kwargs["run_id"], attributes=filters.AttributeFilter(name_eq=attributes))
+    df[run_init_kwargs["run_id"]["test_files/file_destination1"] == "custom_destination.txt"]
+
+@pytest.mark.parametrize(
+    "files",
+    [
+        {},
+        {"test_files/file_error1": ""},
+        {"": "tests/e2e/resources/text_file.txt"},
+        {"test_files/file_error3": "tests/e2e/resources"},  # a directory
+        {"test_files/file_error4": "tests/e2e/resources/does-not-exist"},
+        {"test_files/file_error5": pathlib.Path("tests/e2e/resources/does-not-exist")},
+        {"test_files/file_error6" + "a" * 4 * 1024: "tests/e2e/resources/text_file.txt"},  # file metadata limit
+        {"test_files/file_error_link": "tests/e2e/resources/invalid_link_file"},
+        {"test_files/file_error_large1": b"a" * (20 * 1024 * 1024)},  # is there a size limit?
+    ],
+)
+def test_assign_files_error(run, run_init_kwargs, temp_dir, files):
+    with pytest.raises(ValueError):
+        run.assign_files(files)
+
+
+def test_assign_files_error_no_access(run, run_init_kwargs, temp_dir):
+    # when
+    file_path = temp_dir / "file_no_access"
+    with open(file_path, "w") as f:
+        f.write("test content")
+    os.chmod(file_path, 0o000)  # remove read access
+
+    # then
+    files = {"test_files/file_no_access": file_path}
+    with pytest.raises(ValueError):
+        run.assign_files(files)
+
+def test_assign_files_duplicate(run, run_init_kwargs, temp_dir):
+    files = {"test_files/file_duplicate1": "tests/e2e/resources/text_file.txt"}
+
+    run.assign_files(files)
+    run.assign_files(files)
+    run.wait_for_processing(SYNC_TIMEOUT)
+
+    runs.download_files(runs=run_init_kwargs["run_id"], attributes="test_files/file_duplicate1", destination=temp_dir)
