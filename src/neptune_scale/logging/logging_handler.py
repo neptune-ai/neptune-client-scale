7916b05eab9015c69ef2dcc2288c6f76d76c0b25
Edyta <142720610+szaganek@users.noreply.github.com>
2025-05-15 15:17:51 +0200

    Update docstrings for NeptuneLoggingHandler (#246)
    * Update docstrings for NeptuneLoggingHandler
    
    * Update namespace
    
    Co-authored-by: Sabine Ståhlberg <sabine.stahlberg@neptune.ai>
    
    * Update src/neptune_scale/logging/logging_handler.py
    
    Co-authored-by: Piotr Łusakowski <piotr.lusakowskI@neptune.ai>
    
    * Fix pre-commit failures
    
    ---------
    
    Co-authored-by: Sabine Ståhlberg <sabine.stahlberg@neptune.ai>
    Co-authored-by: Piotr Łusakowski <piotr.lusakowskI@neptune.ai>

--- src/neptune_scale/logging/logging_handler.py
+++ src/neptune_scale/logging/logging_handler.py
@@ -42,11 +42,25 @@ from neptune_scale.sync.parameters import (
 
 class NeptuneLoggingHandler(logging.Handler):
     """
-    A logging handler that sends log messages to a Neptune run.
-    In its constructor, it takes:
-    - a Neptune run to log to,
-    - a logging level (default is NOTSET),
-    - an optional attribute path under which to store logs (default is "system/logs").
+    A logging handler that sends the records created by the Python Logger to a Neptune run.
+
+    Args:
+        run: A reference to a Run object which should track the logs.
+        level: Level of logs to capture. If not provided, defaults to `logging.NOTSET`.
+        attribute_path: Path to the `StringSeries` attribute that stores the logs. If not provided,
+            the logs are stored under "runtime/logs".
+
+    Example:
+        ```
+        from neptune_scale import NeptuneLoggingHandler
+
+        logger = logging.getLogger("my_experiment")
+
+        npt_handler = NeptuneLoggingHandler(run=run, level=logging.INFO, attribute_path="messages/info")
+        logger.addHandler(npt_handler)
+
+        logger.info("Starting data preparation")
+        ```
     """
 
     def __init__(
