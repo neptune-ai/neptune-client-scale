f355777e81165cc6d657ee18be5e0462b6b9294c
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2025-07-08 11:24:20 +0200

    tests: updated docstrings for doctests

--- src/neptune_scale/utils.py
+++ src/neptune_scale/utils.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from typing import Any
 
 
-def stringify_unsupported(d: dict[str, Any], **kwargs: Any) -> dict[str, Any]:
+def stringify_unsupported(d: dict[str, Any], **kwargs: Any) -> dict[str, Any]:  # noqa: C901
     """
     A helper function that flattens a nested dictionary structure and casts unsupported values to strings to be logged in Neptune.
     Note:
@@ -11,23 +11,22 @@ def stringify_unsupported(d: dict[str, Any], **kwargs: Any) -> dict[str, Any]:
 
     Args:
         d: Dictionary to flatten
+        **kwargs: Additional arguments for backward compatibility
 
     Returns:
         dict: Flattened dictionary with string keys and cast values
 
     Example:
+        >>> from neptune_scale import Run
+        >>> run = Run()
         >>> config = {"mixed_nested": {"list": [1, {"a": 2}, 3, None], "dict": {"a": [1, 2], "b": {"c": 3}, "d": None}}}
-        >>> run.log_configs(config)  # without `stringify_unsupported()`
+        >>> # without `stringify_unsupported()`
+        >>> run.log_configs(config)  # doctest: +SKIP
         neptune:WARNING: Dropping value. Config values must be float, bool, int, str, datetime, list, set or tuple (got `mixed_nested`:`{'list': [1, {'a': 2}, 3, None], 'dict': {'a': [1, 2], 'b': {'c': 3}}}`)
-        >>> run.log_configs(stringify_unsupported(config))  # with `stringify_unsupported()`
-        Logged successfully as
-        "mixed_nested": {
-            "list": "[1, {'a': 2}, 3, None]",
-            "dict": {
-                "a": "[1, 2]",
-                "b": {"c": 3},
-            }
-        }
+        >>> # with `stringify_unsupported()`
+        >>> from neptune_scale.utils import stringify_unsupported
+        >>> run.log_configs(stringify_unsupported(config))  # Logged successfully
+
 
 
     For more details, see https://docs.neptune.ai/api/utils/#stringify_unsupported
