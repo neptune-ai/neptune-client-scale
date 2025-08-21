120f73929c5815496f71008ad6c47fb13611b101
Michał Sośnicki <michal.sosnicki@neptune.ai>
2025-08-21 11:46:21 +0200

    fix: empty file and invalid size tests

--- src/neptune_scale/sync/storage/s3.py
+++ src/neptune_scale/sync/storage/s3.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import aiofiles
 import backoff
 import httpx
@@ -44,6 +46,11 @@ async def upload_to_s3(file_path: str, content_type: str, signed_url: str) -> No
 
 async def _upload_file(client: AsyncClient, signed_url: str, file_path: str, content_type: str) -> None:
     # TODO: Implement multipart upload for larger files
+    file_size = Path(file_path).stat().st_size
+    if file_size == 0:
+        await _upload_part(client, signed_url, b"", content_type)
+        return
+
     async with aiofiles.open(file_path, "rb") as file:
         content = await file.read()
         if not content:
@@ -72,7 +79,6 @@ def _is_retryable_httpx_error(exc: Exception) -> bool:
 
 @backoff.on_predicate(backoff.expo, _is_retryable_httpx_error, max_time=HTTP_REQUEST_MAX_TIME_SECONDS)
 async def _upload_part(client: AsyncClient, session_uri: str, content: bytes, content_type: str) -> None:
-    """ """
     # The docs at https://docs.aws.amazon.com/AmazonS3/latest/userguide/PresignedUrlUploadObject.html
     # provide Content-Type as the only header
     headers = {"Content-Type": content_type}
